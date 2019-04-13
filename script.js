e640e87616cd6fd8d6476b515686e7cc8e1fc799
Anya Emmons <anya_volchok@mail.ru>
2019-04-13 20:15:57 +0100

    JS button fix

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
-function showLang() {
-  var langBox = document.getElementsById('langPage');
-  if (langBox.style.display === 'none') {
-    langBox.style.display = 'block';
-  } else {
-    langBox.style.display = 'none';
-  }
-}
+//Uncaught TypeError: Cannot read property 'display' of undefined
+// at showLang (script.js:3)
+// at HTMLButtonElement.onclick (mapview.html:387)
+//
+// function showLang() {
+//   var langBox = document.getElementsByClassName('langPage');
+//   if (langBox.style.display === 'none') {
+//     langBox.style.display = 'block';
+//   } else {
+//     langBox.style.display = 'none';
+//   }
+// }
+//
+// showLang();
 
-showLang();
+//This one works
+$("button").click(function(){
+    $('.langPage').toggle();
+});
+
+// This one works
+// function testFunction() {
+//     var x = document.getElementsByClassName("langPage");
+//     alert(x[0].innerHTML);
+// }
