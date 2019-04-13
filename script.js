11d0a601dab7d223759279bae2804cd6cc1a6e74
Anya Emmons <anya_volchok@mail.ru>
2019-04-13 02:09:12 +0100

    navbar css and js fixes

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function showLang() {
-  var langBox = mapview.getElementsById('langPage');
+  var langBox = document.getElementsById('langPage');
   if (langBox.style.display === 'none') {
     langBox.style.display = 'block';
   } else {
     langBox.style.display = 'none';
   }
 }
+
+showLang();
